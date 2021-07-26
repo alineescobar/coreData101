8f15c0eea568da4e4ba81a7582a82b8f891db19e
Aline Escobar <21069293+alineescobar@users.noreply.github.com>
2021-07-26 00:12:25 -0300

    first steps with core data

---
+++ CoreDataToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataToDoList
+//
+//  Created by Aline Osana Escobar on 25/07/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
